0849ea35bd84d87e6da72a3d51bf91da47e3161d
“jiayun” <jiayun_work@163.com>
2026-08-21 14:57:07 +0800

    fix: scanMinMax 先拷贝再排序，避免写穿 BuildSummary 的起终 SOC

--- internal/report/stats.go
+++ internal/report/stats.go
@@ -3,9 +3,10 @@ package report
 import "battery-soc/internal/parse"
 
 func scanMinMax(trace []float64) (min, max float64) {
-	parse.SortTraceInPlace(trace)
 	if len(trace) == 0 {
 		return 0, 0
 	}
-	return trace[0], trace[len(trace)-1]
+	cp := append([]float64(nil), trace...)
+	parse.SortTraceInPlace(cp)
+	return cp[0], cp[len(cp)-1]
 }
